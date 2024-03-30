020168641ff58ae74eaaf6a48b3ff34d5d82cf51
vinaykumar1332 <vinaypudi1332@gmail.com>
2024-03-30 16:57:07 +0530

    new feature notification added

--- Libraries/exp.js
+++ Libraries/exp.js
@@ -79,3 +79,60 @@ window.onload = function() {
     animateDots();
 };
 
+// --------------------------------------
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Check if the URL contains a hash
+    if (window.location.hash) {
+      // Get the element with the ID matching the hash
+      const targetElement = document.querySelector(window.location.hash);
+      // If the element exists, scroll to it
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  });
+
+
+
+  //-----------------------------
+  // Start the animation when the page loads
+  let rightClickCount = 0;
+  let consecutiveRightClicks = 0;
+  let blockNotificationCount = 0;
+  const maxConsecutiveRightClicks = 5; // Define the maximum number of consecutive right-clicks allowed
+  const maxBlockNotificationCount = 5; // Define the maximum number of block notifications allowed
+  
+  // Function to display notification
+  function displayNotification(notificationId, message) {
+      document.getElementById(notificationId).innerText = message;
+      document.getElementById(notificationId).style.display = 'block';
+  }
+  
+  // Event listener for right-click
+  document.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+      rightClickCount++;
+      consecutiveRightClicks++;
+  
+      if (rightClickCount > 2) {
+          displayNotification("notification1", "Right-clicking is disabled on this website. Please respect the site's content.");
+          setTimeout(function() {
+              document.getElementById('notification1').style.display = 'none';
+          }, 3000); // 3000 milliseconds (3 seconds)
+      }
+  
+      if (consecutiveRightClicks >= maxConsecutiveRightClicks && blockNotificationCount < maxBlockNotificationCount) {
+          displayNotification("notification2", "Your IP will be blocked if you continue to attempt right-clicking.");
+          blockNotificationCount++;
+      } else if (blockNotificationCount >= maxBlockNotificationCount) {
+          displayNotification("notification3", "Your IP has been blocked and reported to the admin team. Please be cautious.");
+          // You can implement further actions here, like IP blocking and reporting
+      }
+  });
+  
+  // Event listener for resetting right-click count
+  document.addEventListener('mousedown', function(e) {
+      if (e.button !== 2) { // Check if it's not a right-click
+          rightClickCount = 0;
+      }
+  });
